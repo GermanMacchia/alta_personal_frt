4ed42977ca9c1255de67ff495371d7150cd41f18
German O. Macchia <germanmacchia@gmail.com>
2023-01-24 13:37:29 -0300

    changes 24-1

--- src/api/apiArea.ts
+++ src/api/apiArea.ts
@@ -1,25 +1,25 @@
-import { sleep } from '../helpers/sleep'
+// import { sleep } from '../helpers/sleep'
 import { api } from './api'
 import { Area } from '../interfaces'
 
 export const fetchAreas = async () => {
-    await sleep( 1 )
+    // await sleep( 1 )
     const { data } = await api.get( '/api/area' )
     return data
 }
 
 export const postAreas = async ( nuevaArea: Area ) => {
     const { nombre } = nuevaArea
-    await sleep( 1 )
+    // await sleep( 1 )
     return await api.post( '/api/area', { nombre } )
 }
 
 export const deleteArea = async ( id: string ) => {
-    await sleep( 1 )
+    // await sleep( 1 )
     return await api.delete( `/api/area/${ id }` )
 }
 
 export const editArea = async ( data: Area ) => {
-    await sleep( 1 )
+    // await sleep( 1 )
     return await api.patch( `/api/area/${ data._id }`, data )
 }
--- src/api/apiEmpleado.ts
+++ src/api/apiEmpleado.ts
@@ -1,24 +1,24 @@
-import { sleep } from "../helpers"
+// import { sleep } from "../helpers"
 import { api } from "./api"
 import { Empleado } from '../interfaces'
 
 export const fetchEmpleados = async () => {
-    await sleep( 1 )
+    // await sleep( 1 )
     const { data } = await api.get( '/api/empleado' )
     return data
 }
 
 export const deleteEmpleado = async ( id: string ) => {
-    await sleep( 1 )
+    // await sleep( 1 )
     return await api.delete( `/api/empleado/${ id }` )
 }
 
 export const editEmpleado = async ( data: Empleado ) => {
-    await sleep( 1 )
+    // await sleep( 1 )
     return await api.patch( `/api/empleado/${ data._id }`, data )
 }
 
 export const postEmpleado = async ( nuevoEmpleado: Empleado ) => {
-    await sleep( 1 )
+    // await sleep( 1 )
     return await api.post( '/api/empleado', nuevoEmpleado )
 }
