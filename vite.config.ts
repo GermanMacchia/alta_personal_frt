2922b011798223623c40ab8d1d94c8fe7c4f6545
German O. Macchia <germanmacchia@gmail.com>
2023-01-24 20:28:25 -0300

    merge conflict

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig( {
   plugins: [ react() ],
-<<<<<<< HEAD
   base: '/alta_personal_frt/'
-=======
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './tests/setup.ts',
-  }
->>>>>>> main
 } )
