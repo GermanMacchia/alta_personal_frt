d40ba96c737aa7dfd042a8ff28e7ab2ae44a5f9c
German O. Macchia <germanmacchia@gmail.com>
2023-01-28 12:19:09 -0300

    card

--- src/components/CardInfo/CardInfo.tsx
+++ src/components/CardInfo/CardInfo.tsx
@@ -11,15 +11,11 @@ import {
 	Card,
 } from '@mui/material'
 import { capitalize } from '../../helpers'
-<<<<<<< HEAD
-import userImage from '../../assets/user-circle.png'
-=======
 import CheckIcon from '@mui/icons-material/Check'
 import NotInterestedIcon from '@mui/icons-material/NotInterested'
 import IconButton from '@mui/material/IconButton/IconButton'
 import CloseIcon from '@mui/icons-material/Close'
->>>>>>> main
-
+import userImage from '../../assets/user-circle.png'
 interface Props {
 	empleado: Empleado
 	areas: Area[]
@@ -28,76 +24,17 @@ interface Props {
 
 export const CardInfo: FC<Props> = ({ empleado, handleClose, areas }) => {
 	return (
-<<<<<<< HEAD
-		<Card sx={styles.container}>
-			<Box sx={styles.container.column}>
-				<CardMedia
-					component='img'
-					image={userImage}
-					alt='user circle'
-					sx={styles.container.image}
-				/>
-				<Box sx={{ margin: '1rem' }}>
-					<Typography gutterBottom variant='h5' component='div'>
-						{capitalize(empleado.nombre) + ' ' + capitalize(empleado.apellido)}
-					</Typography>
-					<Typography gutterBottom variant='body2' component='div'>
-						<b>DNI:</b> {empleado.dni}
-					</Typography>
-					<Typography gutterBottom variant='body2' component='div'>
-						<b>FNac:</b> {new Date(empleado.fechaNac).toLocaleDateString()}
-					</Typography>
-					<Typography gutterBottom variant='body2' component='div'>
-						<b>Es Desarrollador:</b> {empleado.esDesarrollador ? 'Si' : 'No'}
-					</Typography>
-				</Box>
-			</Box>
-			<Box sx={{ ...styles.container.column, ...styles.container.right }}>
-				<CardContent>
-					<Typography gutterBottom variant='body2'>
-						<p>
-							<b>Descripción:</b>
-						</p>
-						{empleado.descripcion}
-					</Typography>
-					<Typography gutterBottom variant='body2'>
-						<p>
-							<b>Área:</b>
-						</p>
-						{areas
-							.find((area: Area) => area._id === empleado.area)
-							?.nombre.toUpperCase()}
-					</Typography>
-					<Typography gutterBottom maxHeight='10px' variant='body2'>
-						<p>
-							<b>Otros:</b>
-						</p>
-						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error maiores
-						necessitatibus, impedit
-					</Typography>
-				</CardContent>
-				<CardActions sx={styles.container.actions}>
-					<Button
-						variant='outlined'
-						color='success'
-						onClick={handleClose}
-						size='small'>
-						Cerrar
-					</Button>
-				</CardActions>
-=======
 		<>
 			<Box sx={{ display: 'flex', justifyContent: 'end' }}>
 				<IconButton onClick={handleClose}>
 					<CloseIcon color='error' sx={{ fontSize: '35px' }} />
 				</IconButton>
->>>>>>> main
 			</Box>
 			<Card sx={styles.container}>
 				<Box sx={styles.container.column}>
 					<CardMedia
 						component='img'
-						image='/src/assets/user-circle.png'
+						image={userImage}
 						alt='user circle'
 						sx={styles.container.image}
 					/>
